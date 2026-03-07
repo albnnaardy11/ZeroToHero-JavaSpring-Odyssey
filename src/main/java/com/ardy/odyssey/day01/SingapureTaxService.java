a4a5de0334c8a278bf4beceba7eeee28a61557e5
albnnaardy11 <albnnaardy@gmail.com>
2026-03-07 09:24:22 +0700

    docs: add README.md for the Day 01 Singapore Tax Calculator module.

--- src/main/java/com/ardy/odyssey/day01/SingapureTaxService.java
+++ src/main/java/com/ardy/odyssey/day01/SingapureTaxService.java
@@ -1,5 +1,8 @@
 package com.ardy.odyssey.day01;
 
+import com.ardy.odyssey.day01.TaxCalculator;
+import com.ardy.odyssey.day01.TaxPayer;
+
 public class SingapureTaxService extends  TaxPayer implements TaxCalculator{
 
     public SingapureTaxService(String name, String taxId) {
