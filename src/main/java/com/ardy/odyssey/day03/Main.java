fe62a63a2b93f9f1879de7cc8628edd2a79e1390
albnnaardy11 <albnnaardy@gmail.com>
2026-03-09 21:26:29 +0700

    feat: implement initial camera rental service with stock and customer queue management.

--- src/main/java/com/ardy/odyssey/day03/Main.java
+++ src/main/java/com/ardy/odyssey/day03/Main.java
@@ -6,18 +6,29 @@ import com.ardy.odyssey.day03.model.Kamera;
 public class Main {
     public static void main(String[] args){
 
-        Kamera cam = new Kamera("Cam-01", "Sony A7IV", 200.00, "Full Frame");
-
+        Kamera cam1 = new Kamera("Cam-01", "Sony A7IV", 200.0, "Full Frame");
+        Kamera cam2 = new Kamera("Cam-02", "Canon R6", 180.0, "Full Frame");
         LayananSewa layananSewa = new LayananSewa();
 
-        System.out.println("--- Detail Barang ---");
-        cam.tampilkanDetail();
+        layananSewa.tambahKameraKeStok(cam1);
+        layananSewa.tambahKameraKeStok(cam2);
 
-        int durasiSewa = 5;
-        double totalBiaya = layananSewa.hitungTotal(cam, durasiSewa);
+        System.out.println("--- Pendaftaran Antrian ---");
+        layananSewa.tambahKeAntrian("Ardy");
+        layananSewa.tambahKeAntrian("Budi");
 
-        System.out.println("\n--- Ringkasan Sewa ---");
-        System.out.println("Durasi: " + durasiSewa + "hari");
-        System.out.println("Total yang dibayar: $" + totalBiaya);
+        System.out.println("\n--- Proses Sewa ---");
+        layananSewa.layananPenyewa();
+        cam1.tampilkanDetail();
+
+        int durasiSewa = 3;
+        double totalBiaya = layananSewa.hitungTotal(cam1, durasiSewa);
+        System.out.println("Total Biaya: $" + totalBiaya);
+
+        // 5. Test Hapus Stok (Iterator)
+        System.out.println("\n--- Update Inventaris ---");
+        layananSewa.hapusKameraDariStok("Cam-01");
+        
+        layananSewa.layananPenyewa();
     }
 }
--- src/main/java/com/ardy/odyssey/day03/service/LayananSewa.java
+++ src/main/java/com/ardy/odyssey/day03/service/LayananSewa.java
@@ -1,9 +1,49 @@
 package com.ardy.odyssey.day03.service;
 
 import com.ardy.odyssey.day03.model.ItemSewa;
+import com.ardy.odyssey.day03.model.Kamera;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class LayananSewa {
+
+    private  Queue<String> antrianPenyewa = new LinkedList<>();
+    private  Set<Kamera> daftarStok = new HashSet<>();
+
+    public void tambahKeAntrian(String nama){
+        antrianPenyewa.add(nama);
+        System.out.println("Masuk ke dalam antrian.");
+    }
+
+    public void layananPenyewa() {
+        String penyewa = antrianPenyewa.poll();
+        if (penyewa != null){
+            System.out.println("Melayani penyewa: " + penyewa);
+        } else {
+            System.out.println("Antrian kosong!");
+        }
+    }
+
+    public void tambahKameraKeStok(Kamera kamera){
+        daftarStok.add(kamera);
+    }
+
+    public void hapusKameraDariStok(String id) {
+        Iterator<Kamera> it = daftarStok.iterator();
+        while (it.hasNext()) {
+            Kamera k = it.next();
+            if (k.getId().equals(id)) {
+                it.remove();
+                System.out.println("Kamera" + id + "dihapus dari stok.");
+
+            }
+        }
+    }
     public double hitungTotal(ItemSewa item, int durasi){
-        return item.getHargaPerHari() * durasi;
+    return item.getHargaPerHari() * durasi;
     }
 }
